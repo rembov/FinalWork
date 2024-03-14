649aa0cd6d39795bd7585a537fcea1bab7110701
Kadetys <ku4in2005@gmail.com>
2024-03-14 15:12:12 +0300

    negriiiiiiiii

--- main1.c
+++ main1.c
@@ -24,7 +24,7 @@ int main()
     scanf_s("%lf", &xl);
     s1 = log(xl) + ((2 + xl) / pow(xl, 2));
     s2 = tan(xl) - (1 / sqrt(pow(xl, 2) + 10));
-    y = s1 / s2;
+    y = (s1 / s2)-x1;
     printf("AAAAAAAAAA NEGRI %lf", y);
     getchar();
     return 0;
