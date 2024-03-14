8b74c5737731d221ad1c73bc88b59f1d99d96364
Buster Daemon <bugsbunnygdi@pm.me>
2024-03-14 15:00:13 +0300

    Добавлено деление s2

--- main1.c
+++ main1.c
@@ -23,7 +23,7 @@ int main()
     printf("Add x: ");
     scanf_s("%lf", &xl);
     s1 = log(xl) + ((2 + xl) / pow(xl, 2));
-    s2 = tan(xl) - (1 / sqrt(pow(xl, 2) + 10));
+    s2 = tan(xl) / 3;
     y = s1 / s2;
     printf("%lf", y);
     getchar();
